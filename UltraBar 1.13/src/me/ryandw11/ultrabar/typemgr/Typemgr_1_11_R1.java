fb2c60026be15a4fb9f5870d5af32878d3715637
montlikadani <montlikada@gmail.com>
2018-08-01 17:52:56 +0200

    Update Typemgr_1_11_R1.java

--- UltraBar 1.13/src/me/ryandw11/ultrabar/typemgr/Typemgr_1_11_R1.java
+++ UltraBar 1.13/src/me/ryandw11/ultrabar/typemgr/Typemgr_1_11_R1.java
@@ -1,6 +1,5 @@
 package me.ryandw11.ultrabar.typemgr;
 
-
 import net.minecraft.server.v1_11_R1.IChatBaseComponent;
 import net.minecraft.server.v1_11_R1.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_11_R1.PacketPlayOutChat;
@@ -10,12 +9,9 @@ import org.bukkit.entity.Player;
 
 public class Typemgr_1_11_R1 implements Typemgr{
 
-	
-
-	
 	public Typemgr_1_11_R1(){
 	}
-	
+
 	/*Bar Style method created.
 	 * 
 	 * # of Notched lines.
@@ -25,6 +21,7 @@ public class Typemgr_1_11_R1 implements Typemgr{
 	public void title(String message, Player p, int fadein, int time, int fadeout, String subtitle){
 		p.sendTitle(message, subtitle); //fixes error in 1.11
 	}
+
 	@Override
 	public void actionBar(Player player, String message){
 		CraftPlayer p = (CraftPlayer) player;
@@ -32,8 +29,5 @@ public class Typemgr_1_11_R1 implements Typemgr{
 		PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, (byte) 2);
         ((CraftPlayer) p).getHandle().playerConnection.sendPacket(ppoc);
 	}
-	
-	
-	
-	
+
 }

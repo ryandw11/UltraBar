3867dd55e64a91cd95b0c1d0e51db761826378f1
montlikadani <montlikada@gmail.com>
2018-08-01 17:33:08 +0200

    Update ActionBarCommands.java

--- UltraBar 1.13/src/me/ryandw11/ultrabar/commands/ActionBarCommands.java
+++ UltraBar 1.13/src/me/ryandw11/ultrabar/commands/ActionBarCommands.java
@@ -1,8 +1,7 @@
 package me.ryandw11.ultrabar.commands;
 
-import net.md_5.bungee.api.ChatColor;
-
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +10,9 @@ import org.bukkit.entity.Player;
 import me.ryandw11.ultrabar.core.UltraBar;
 
 public class ActionBarCommands implements CommandExecutor {
-	
+
 	private UltraBar plugin;
+
 	public ActionBarCommands(UltraBar plugin){
 		this.plugin = plugin;
 	}
@@ -34,7 +34,7 @@ public class ActionBarCommands implements CommandExecutor {
 							Message = Message + " " + args[i];
 						}
 						Player p2 = (Player) Bukkit.getServer().getPlayer(args[1]);
-						plugin.mgr.actionBar(p2, Message.replace("&", "ง"));
+						plugin.mgr.actionBar(p2, Message.replace("&", "ยง"));
 					}
 					else{
 						p.sendMessage(ChatColor.RED + "Invalid usage: /actionbar send (player) (message)");
@@ -47,13 +47,13 @@ public class ActionBarCommands implements CommandExecutor {
 			else if(args[0].equalsIgnoreCase("sendall")){
 				if(p.hasPermission("ultrabar.actionbar.sendall")){
 					if(args.length >= 2){
-					
+
 						String Message = "";
 						for (int i = 1; i < args.length; i++){
 						Message = Message + " " + args[i];
 						}
 						for(Player p1 : Bukkit.getOnlinePlayers()){
-						plugin.mgr.actionBar(p1,(Message.replace("&", "ง")));
+						plugin.mgr.actionBar(p1,(Message.replace("&", "ยง")));
 						}	
 					}
 					else{
